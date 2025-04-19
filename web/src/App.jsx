f58a12cce9ab51261b28af7ec790b9cbadbf0b49
Cusox <cusoxlee@gmail.com>
2025-04-19 23:14:46 +0800

    fix: button bug

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -246,7 +246,7 @@ const ProjectShowcase = () => {
                         </select>
                     </div>
 		    <button
-		        clicked={handleRandomRepo}
+			onClick={handleRandomRepo}
 		        title="Open a random repo"
 		        className="px-4 py-2 border border-gray-300 rounded-lg bg-white text-gray-700 text-sm font-medium hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-colors whitespace-nowrap shadow-sm"
 		    >
